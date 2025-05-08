036343b8ba488bb12cadfade76fb1b2eaec79282
Justin Bakse <justin@tsfim.com>
2025-05-08 13:13:56 -0500

    untrack generated assets

--- scripts/run-aseprite.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-ASEPRITE="/Users/anacatharina/Library/Application Support/Steam/steamapps/common/Aseprite/Aseprite.app/Contents/MacOS/aseprite"
-
-"$ASEPRITE" -b "$@"
--- src/js/assets.js
+++ src/js/assets.js
@@ -8,108 +8,108 @@ const assetsQueue = [];
 export function preload() {
   assets.player1 = {
     left: {
-      base: loadImage("assets/players/player1/left/base.png"),
-      light: loadImage("assets/players/player1/left/light.png"),
+      base: loadImage("generated/players/player1/left/base.png"),
+      light: loadImage("generated/players/player1/left/light.png"),
     },
     right: {
-      base: loadImage("assets/players/player1/right/base.png"),
-      light: loadImage("assets/players/player1/right/light.png"),
+      base: loadImage("generated/players/player1/right/base.png"),
+      light: loadImage("generated/players/player1/right/light.png"),
     },
     up: {
-      base: loadImage("assets/players/player1/up/base.png"),
-      light: loadImage("assets/players/player1/up/light.png"),
+      base: loadImage("generated/players/player1/up/base.png"),
+      light: loadImage("generated/players/player1/up/light.png"),
     },
     down: {
-      base: loadImage("assets/players/player1/down/base.png"),
-      light: loadImage("assets/players/player1/down/light.png"),
+      base: loadImage("generated/players/player1/down/base.png"),
+      light: loadImage("generated/players/player1/down/light.png"),
     },
   };
   assets.player2 = {
     left: {
-      base: loadImage("assets/players/player2/left/base.png"),
-      light: loadImage("assets/players/player2/left/light.png"),
+      base: loadImage("generated/players/player2/left/base.png"),
+      light: loadImage("generated/players/player2/left/light.png"),
     },
     right: {
-      base: loadImage("assets/players/player2/right/base.png"),
-      light: loadImage("assets/players/player2/right/light.png"),
+      base: loadImage("generated/players/player2/right/base.png"),
+      light: loadImage("generated/players/player2/right/light.png"),
     },
     up: {
-      base: loadImage("assets/players/player2/up/base.png"),
-      light: loadImage("assets/players/player2/up/light.png"),
+      base: loadImage("generated/players/player2/up/base.png"),
+      light: loadImage("generated/players/player2/up/light.png"),
     },
     down: {
-      base: loadImage("assets/players/player2/down/base.png"),
-      light: loadImage("assets/players/player2/down/light.png"),
+      base: loadImage("generated/players/player2/down/base.png"),
+      light: loadImage("generated/players/player2/down/light.png"),
     },
   };
-  assets.tileMaps = [loadImage("assets/tile_map/1.png"), loadImage("assets/tile_map/2.png")];
+  assets.tileMaps = [loadImage("generated/tile_map/1.png"), loadImage("generated/tile_map/2.png")];
 
   assets.items = {
     crate: [
       {
-        base: loadImage("assets/items/crystals/1/base.png"),
-        light: loadImage("assets/items/crystals/1/light.png"),
+        base: loadImage("generated/items/crystals/1/base.png"),
+        light: loadImage("generated/items/crystals/1/light.png"),
       },
       {
-        base: loadImage("assets/items/crystals/2/base.png"),
-        light: loadImage("assets/items/crystals/2/light.png"),
+        base: loadImage("generated/items/crystals/2/base.png"),
+        light: loadImage("generated/items/crystals/2/light.png"),
       },
       {
-        base: loadImage("assets/items/crystals/3/base.png"),
-        light: loadImage("assets/items/crystals/3/light.png"),
+        base: loadImage("generated/items/crystals/3/base.png"),
+        light: loadImage("generated/items/crystals/3/light.png"),
       },
     ],
     door: {
-      open: loadImage("assets/items/bridge/open.png"),
-      closed: loadImage("assets/items/bridge/closed.png"),
+      open: loadImage("generated/items/bridge/open.png"),
+      closed: loadImage("generated/items/bridge/closed.png"),
     },
     floorSwitch: {
-      up: loadImage("assets/items/switch/up.png"),
-      down: loadImage("assets/items/switch/down.png"),
+      up: loadImage("generated/items/switch/up.png"),
+      down: loadImage("generated/items/switch/down.png"),
     },
     bullet: {
-      player1: loadImage("assets/items/bullet/player1.png"),
-      player2: loadImage("assets/items/bullet/player2.png"),
+      player1: loadImage("generated/items/bullet/player1.png"),
+      player2: loadImage("generated/items/bullet/player2.png"),
     },
     stairs: {
       up: {
-        base: loadImage("assets/items/stairs/up/base.png"),
-        light: loadImage("assets/items/stairs/up/light.png"),
+        base: loadImage("generated/items/stairs/up/base.png"),
+        light: loadImage("generated/items/stairs/up/light.png"),
       },
-      down: loadImage("assets/items/stairs/down.png"),
+      down: loadImage("generated/items/stairs/down.png"),
     },
-    treasure: loadImage("assets/items/treasure.png"),
+    treasure: loadImage("generated/items/treasure.png"),
   };
 
   assets.ground = {
     darkest: [
-      loadImage("assets/ground/darkest/1.png"),
-      loadImage("assets/ground/darkest/2.png"),
-      loadImage("assets/ground/darkest/3.png"),
+      loadImage("generated/ground/darkest/1.png"),
+      loadImage("generated/ground/darkest/2.png"),
+      loadImage("generated/ground/darkest/3.png"),
     ],
     dark: [
-      loadImage("assets/ground/dark/1.png"),
-      loadImage("assets/ground/dark/2.png"),
-      loadImage("assets/ground/dark/3.png"),
+      loadImage("generated/ground/dark/1.png"),
+      loadImage("generated/ground/dark/2.png"),
+      loadImage("generated/ground/dark/3.png"),
     ],
     light: [
-      loadImage("assets/ground/light/1.png"),
-      loadImage("assets/ground/light/2.png"),
-      loadImage("assets/ground/light/3.png"),
-      loadImage("assets/ground/light/4.png"),
-      loadImage("assets/ground/light/5.png"),
-      loadImage("assets/ground/light/6.png"),
-      loadImage("assets/ground/light/7.png"),
-      loadImage("assets/ground/light/8.png"),
+      loadImage("generated/ground/light/1.png"),
+      loadImage("generated/ground/light/2.png"),
+      loadImage("generated/ground/light/3.png"),
+      loadImage("generated/ground/light/4.png"),
+      loadImage("generated/ground/light/5.png"),
+      loadImage("generated/ground/light/6.png"),
+      loadImage("generated/ground/light/7.png"),
+      loadImage("generated/ground/light/8.png"),
     ],
     lightest: [
-      loadImage("assets/ground/lightest/1.png"),
-      loadImage("assets/ground/lightest/2.png"),
-      loadImage("assets/ground/lightest/3.png"),
+      loadImage("generated/ground/lightest/1.png"),
+      loadImage("generated/ground/lightest/2.png"),
+      loadImage("generated/ground/lightest/3.png"),
     ],
     highlight: [
-      loadImage("assets/ground/highlight/1.png"),
-      loadImage("assets/ground/highlight/2.png"),
+      loadImage("generated/ground/highlight/1.png"),
+      loadImage("generated/ground/highlight/2.png"),
     ],
   };
 }
